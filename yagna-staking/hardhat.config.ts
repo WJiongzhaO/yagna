77818cc2e49a0f2ccb8e53227233955d72b0aecc
Wing_WR <wingwr998@gmail.com>
2025-12-30 13:12:25 +0800

    fix:  修改hardhat版本

--- yagna-staking/hardhat.config.ts
+++ yagna-staking/hardhat.config.ts
@@ -2,7 +2,6 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv";
 
-// 加载 .env 文件中的环境变量
 dotenv.config();
 
 const config: HardhatUserConfig = {
@@ -16,17 +15,13 @@ const config: HardhatUserConfig = {
     },
   },
   networks: {
-    // 修复错误 2322: 不能为空对象，需指定 type
     hardhat: {
-      // @ts-ignore: 这里的 type 取决于你安装的具体插件要求，通常为 edr-simulated
-      type: "edr-simulated", 
     },
-    // 修复错误 2322: 必须包含 type: "http"
     amoy: {
-      // @ts-ignore: 显式声明类型以符合你的环境校验
-      type: "http",
       url: process.env.AMOY_RPC_URL || "https://rpc-amoy.polygon.technology/",
-      accounts: process.env.AMOY_PRIVATE_KEY ? [process.env.AMOY_PRIVATE_KEY] : [],
+      accounts: process.env.AMOY_PRIVATE_KEY
+        ? [process.env.AMOY_PRIVATE_KEY]
+        : [],
     },
   },
 };
